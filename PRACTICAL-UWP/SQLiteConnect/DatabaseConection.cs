59ceb5a09b3bc8954129dcea3e3f33ca3094c060
ducbe <ducbe2k2@gmail.com>
2021-11-09 10:10:44 +0700

    commit

---
+++ PRACTICAL-UWP/SQLiteConnect/DatabaseConection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace PRACTICAL_UWP.SQLiteConnect
+{
+    public static class DatabaseConection
+    {
+        public async static void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("contact_db.db", CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "contact_db.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                var tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS contact (id INTEGER PRIMARY KEY, " +
+                    "name NVARCHAR(255))" + "phone_number NVACHAR(255)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+
+    }
+}
---
+++ PRACTICAL-UWP/Services/ContactServices.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Data.Sqlite;
+using PRACTICAL_UWP.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace PRACTICAL_UWP.Services
+{
+    public class ContactServices
+    {
+        public static void Save(string inputText)
+        {
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+        public static List<Contact> GetData()
+        {
+            List<Contact> contact = new List<Contact>();
+
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                 
+                }
+            }
+
+            return contact;
+        }
+    }
+}
